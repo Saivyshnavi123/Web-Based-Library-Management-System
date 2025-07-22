0757fbb1f44a76ab1efc3c42f14a576ea6a03b0c
saichowdhary <saichowdhary08@gmail.com>
2025-07-22 17:33:59 +0100

    update url

--- frontend/src/api/ApiProvider.jsx
+++ frontend/src/api/ApiProvider.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://library-assignment-7psr.onrender.com";
+export const API_BASE_URL = "https://web-based-library-management-system.onrender.com/";
+//export const API_BASE_URL = "http://127.0.0.1:5000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
